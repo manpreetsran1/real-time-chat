1e8b822ae0ae2ecfc4d8c098eefc7162b9faa40c
manpreetsran1 <sranmanpreet881@gmail.com>
2021-11-16 22:54:50 +0530

    one to one

--- index.js
+++ index.js
@@ -17,14 +17,40 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  chatID = socket.handshake.query.chatID
+    socket.join(chatID)
+
+    // console.log(socket.handshake, "chatID");
+
+
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+
+    
   });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+  // socket.on("send_message", (data) => {
+  //   socket.to(data.room).emit("receive_message", data);
+
+  //   console.log(data, "senddata");
+  // });
+
+
+  socket.on('send_message', message => {
+    receiverChatID = message.receiverChatID
+    senderName = message.author,
+    senderChatID = message.senderChatID
+    content = message.message
+console.log(message,"messae detail");
+    //Send message to only that particular room
+    socket.in(receiverChatID).emit('receive_message', {
+        'content': content,
+        'senderChatID': senderChatID,
+        'receiverChatID':receiverChatID,
+        "senderName": senderName,
+    })
+})
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
